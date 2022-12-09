b6286d51c4917869206317fdfe7d1612143614c9
renwish <eugenebasinio@gmail.com>
2022-12-10 06:31:45 +0800

    maze print function and maze start finder added

--- pathfindingpyth/pathfindingpyth.py
+++ pathfindingpyth/pathfindingpyth.py
@@ -3,4 +3,34 @@ from curses import wrapper
 import queue
 import time
 
+maze = [
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", "#", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", " ", " ", "#", " ", " ", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if (i, j) in path:
+                stdscr.addstr(i, j*2, "X", RED)
+            else:
+                stdscr.addstr(i, j*2, value, BLUE)
+
+
+def find_start(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j
+
+    return None
